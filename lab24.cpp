85bde97d65e3c4b6b5f53873e4c3656ffd4cab77
Lolorpof <lolorpof@gmail.com>
2023-03-07 16:57:10 +0700

    lab24 completed

--- lab24.cpp
+++ lab24.cpp
@@ -29,10 +29,71 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 }
 
 //Write your code here
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber(real*c.real - imag*c.imag, real*c.imag + imag*c.real);
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	return ComplexNumber((real*c.real + imag*c.imag)/(pow(c.real, 2) + pow(c.imag, 2)), (imag*c.real - real*c.imag)/(pow(c.real, 2) + pow(c.imag, 2)));
+}
+
+ComplexNumber operator+(double a, const ComplexNumber &c){
+	return ComplexNumber(a+c.real,c.imag);
+}
+
+ComplexNumber operator-(double a,const ComplexNumber &c){
+	return ComplexNumber(a-c.real, -c.imag);
+}
+
+ComplexNumber operator*(double a,const ComplexNumber &c){
+	return ComplexNumber(a*c.real,a*c.imag);
+}
+
+ComplexNumber operator/(double a,const ComplexNumber &c){
+	return ComplexNumber((a*c.real)/(pow(c.real, 2) + pow(c.imag, 2)), (-1*a*c.imag)/(pow(c.real, 2) + pow(c.imag, 2)));
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if (real == c.real && imag == c.imag){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+bool operator==(double a, const ComplexNumber &c){
+	if (a == c.real && a == c.imag){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+double ComplexNumber::abs(){
+	return sqrt(pow(real, 2) + pow(imag, 2));
+}
+
+double ComplexNumber::angle(){
+	return atan2(imag, real) * 180/M_PI;
+}
+
+ostream & operator<<(ostream &os, const ComplexNumber &c){
+	char t;
+	t = c.imag >= 0 ? '+' : '-';
+	if (c.real != 0 && c.imag != 0)
+		return os << c.real << t << abs(c.imag) << "i";
+	else if (c.real == 0 && c.imag != 0)
+		return os << c.imag << "i";
+	else
+		return os << c.real;
+}
+
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
-	cout << a << "\n";
+	/*cout << a << "\n";
 	cout << b << "\n";
 	cout << c << "\n";
 	cout << a+2.5 << "\n";
@@ -41,7 +102,7 @@ int main(){
 	cout << 1.5-a << "\n";
 	cout << b+ComplexNumber(0,2.5) << "\n";
 	cout << c-c << "\n";
-	cout << "-----------------------------------\n";
+	cout << "-----------------------------------\n";*/
 	
 	ComplexNumber d = (a+b)/c;
 	ComplexNumber e = b/(a-c);
@@ -52,7 +113,7 @@ int main(){
 	cout << 1/c << "\n";
 	cout << "-----------------------------------\n";
 	
-	cout << ComplexNumber(1,1).abs() << "\n";
+	/*cout << ComplexNumber(1,1).abs() << "\n";
 	cout << ComplexNumber(-1,1).abs() << "\n";
 	cout << ComplexNumber(1.5,2.4).abs() << "\n";
 	cout << ComplexNumber(3,4).abs() << "\n";
@@ -68,7 +129,7 @@ int main(){
 	
 	cout << (ComplexNumber(1,1) == ComplexNumber(1,2)) << "\n";
 	cout << (ComplexNumber(1,1) == 1) << "\n";
-	cout << (0 == ComplexNumber()) << "\n";
+	cout << (0 == ComplexNumber()) << "\n";*/
 }
 
 
